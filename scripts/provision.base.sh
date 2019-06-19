7ab576911c662ab5639d831066cbe7cc5b149ae7
Florian Limberger <flo@snakeoilproductions.net>
2019-06-19 14:21:31 +0200

    Initial commit: Boxes for build host and development box

---
+++ scripts/provision.base.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+set -e
+set -u
+
+hostname="$1"
+
+if [ ! -n "$hostname" ]
+then
+	echo "fatal: no hostname provided" >&2
+	exit 1
+fi
+
+# set hostname
+sysrc hostname="$hostname"
+# also set it for first boot
+hostname "$hostname"
+
+# change home directory
+cd /
+mv /home/vagrant /usr/home/
+rmdir /home
+ln -s /usr/home /home
+pw usermod vagrant -d /usr/home/vagrant
+cd
+
+# set UTF-8
+cat <<EOF >>/etc/login.conf
+utf8|UTF-8 Encoding:\\
+:charset=utf-8:\\
+:lang=en_US.UTF-8:\\
+:tc=default:
+EOF
+cap_mkdb /etc/login.conf
+pw usermod vagrant -L utf8
+
+# change the package repo from ``quaterly'' to ``latest''
+mkdir -p /usr/local/etc/pkg/repos
+sed s:quarterly:latest: </etc/pkg/FreeBSD.conf >/usr/local/etc/pkg/repos/FreeBSD.conf
+
+# install software
+pkg update
+
+# install salt
+pkg install -y py27-salt
+
+# general tools
+pkg install -y git mksh tmux vim-lite
+
+# the vm will be used interactively, so change the default shell
+pw usermod vagrant -s /usr/local/bin/mksh
